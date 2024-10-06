293d6fea140c4b95ed25fa69250a0a3d02a980e9
Nicholas Ragland <nragland37@gmail.com>
2024-10-06 00:30:59 -0500

    remove redundant commands

--- src/components/head.js
+++ src/components/head.js
@@ -59,7 +59,7 @@ const Head = ({ title, description, image }) => {
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />      
+      <meta name="twitter:image" content={seo.image} />
     </Helmet>
   );
 };
