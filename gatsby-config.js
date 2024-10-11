92769f1b368b37ce69aac129fee6c763b23a0dc1
Nicholas Ragland <nragland37@gmail.com>
2024-10-11 06:48:57 -0500

    replaced outdated .ico - not supported

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: config.colors.zeus,
         theme_color: config.colors.green,
         display: 'minimal-ui',
-        icon: 'static/favicon.ico',
+        icon: 'static/web-app-manifest-512x512.png',
         icons: [
           {
             src: 'static/favicon-16x16.png',
@@ -43,7 +43,7 @@ module.exports = {
             type: 'image/png',
           },
           {
-            src: 'static/android-chrome-512x512.png',
+            src: 'static/web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
@@ -92,18 +92,18 @@ module.exports = {
             // https://www.gatsbyjs.org/packages/gatsby-remark-external-links
             resolve: 'gatsby-remark-external-links',
             options: {
-              target: '_blank',
-              rel: 'nofollow noopener noreferrer',
+              target: '_blank', // Ensures external links open in a new tab
+              rel: 'nofollow noopener noreferrer', // Improves security for external links
             },
           },
           {
             // https://www.gatsbyjs.org/packages/gatsby-remark-images
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 700,
-              linkImagesToOriginal: true,
-              quality: 90,
-              tracedSVG: { color: config.colors.green },
+              maxWidth: 700, // Maximum width for images
+              linkImagesToOriginal: true, // Links images to their original size
+              quality: 90, // Image quality
+              tracedSVG: { color: config.colors.green }, // Optional traced SVG placeholder with green color
             },
           },
           {
@@ -124,12 +124,10 @@ module.exports = {
               classPrefix: 'language-',
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
+              // This separator is a string and will do no white-space stripping.
+              // A suggested value for English speakers is the non-ascii character '›'.
               inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
+              // This lets you set up language aliases. For example,
               // setting this to '{ sh: "bash" }' will let you use
               // the language "sh" which will highlight using the
               // bash highlighter.
@@ -137,7 +135,7 @@ module.exports = {
               // This toggles the display of line numbers globally alongside the code.
               // To use it, add the following line in gatsby-browser.js
               // right after importing the prism color scheme:
-              //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+              // require("prismjs/plugins/line-numbers/prism-line-numbers.css")
               // Defaults to false.
               // If you wish to only show line numbers on certain code blocks,
               // leave false and use the {numberLines: true} syntax below
