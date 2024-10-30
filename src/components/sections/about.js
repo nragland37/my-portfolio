271a34c77ea5a7cb09a8d0bfb2d43851bc329f02
Nicholas Ragland <nragland37@gmail.com>
2024-10-30 07:44:40 -0500

    add new pic

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -212,7 +212,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me2.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
